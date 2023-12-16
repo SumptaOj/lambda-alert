2c32151e95f8c3ed333ecf3afa7bf67a57eb4532
Assumpta Ojukwu <assumptaojukwu.com>
2023-12-16 16:33:17 +0100

    first commit

--- lib/cdk-workshop-ts-stack.ts
+++ lib/cdk-workshop-ts-stack.ts
@@ -1,19 +1,37 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import { HitCounter } from './hitcounter';
+import { LambdaTimeoutAlertConstruct } from './lambda-alert'
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopTsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopTsQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // Defines Aws Lambda Function
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTsTopic');
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Define AWS API Gateway REST API resource backed by hello function
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
+    });
+
+    new LambdaTimeoutAlertConstruct(this, 'LamdaAlert');
+  
   }
 }
---
+++ lib/lambda-alert.ts
@@ -0,0 +1,44 @@
+import * as cdk from '@aws-cdk/core';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as snsSubscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as actions from "aws-cdk-lib/aws-cloudwatch-actions";
+import * as lambdaSDK from 'aws-sdk';
+import { Construct } from 'constructs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+
+export class LambdaTimeoutAlertConstruct extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+
+    const lambdaClient = new lambdaSDK.Lambda();
+    const allFunctions = lambdaClient.listFunctions().promise();
+
+    const snsTopic = new sns.Topic(this, 'Lmada-sns-topic');
+    snsTopic.addSubscription(new snsSubscriptions.EmailSubscription('summioj@gmail.com'));
+
+    allFunctions?.then(functions => {
+      functions?.Functions?.forEach(lamdaFunction=> {
+        const existingFunction = lambda.Function.fromFunctionArn(
+          this,
+          `LamdaFunction-${lamdaFunction.FunctionName}`, 
+          lamdaFunction?.FunctionArn || ""
+        );
+
+        const durationAlarm = new cloudwatch.Alarm(this, `LamdaDurationAlarm-${lamdaFunction.FunctionName}`, {
+          metric: existingFunction.metricDuration(), // Monitor function duration metric
+          threshold: 10, // Set your threshold for timeout
+          evaluationPeriods: 1,
+          actionsEnabled: true,
+        });
+        
+        durationAlarm.addAlarmAction(
+          new actions.SnsAction(snsTopic)
+            );
+
+      });
+    });
+  }
+}
